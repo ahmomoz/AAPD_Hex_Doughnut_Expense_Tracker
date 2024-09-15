79449680fb66afdd43e008338db89d6bcc945ede
ahmomoz <x5426854268@gmail.com>
2024-09-15 09:50:44 +0800

    Updates

---
+++ assets/review-expense-2b37c406.js
@@ -0,0 +1,1 @@
+import"./main-d49edd8d.js";import"./pie-chart-1e830aac.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
--- assets/review-expense-d2dc636c.js
+++
@@ -1,1 +0,0 @@
-import"./main-6f7c25b6.js";import"./pie-chart-fb69ad2c.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
