47026119ca8002a538725e4327550d111346c415
ahmomoz <x5426854268@gmail.com>
2024-09-17 00:36:17 +0800

    Updates

--- assets/review-expense-809b8b15.js
+++
@@ -1,1 +0,0 @@
-import"./main-bbb37db7.js";import"./pie-chart-1e830aac.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
