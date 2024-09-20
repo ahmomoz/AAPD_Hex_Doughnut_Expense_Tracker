f593c2858e50403f5662c0073cf46371dc0aca9a
ahmomoz <x5426854268@gmail.com>
2024-09-20 18:52:09 +0800

    Updates

--- assets/review-expense-8f53deb9.js
+++
@@ -1,1 +0,0 @@
-import"./main-7b8b6d3e.js";import"./pie-chart-687fa0d0.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
