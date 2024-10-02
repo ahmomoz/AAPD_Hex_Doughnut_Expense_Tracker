4ac281286c94154356fdfed3e1794cd78047e6ed
ahmomoz <x5426854268@gmail.com>
2024-10-02 23:17:50 +0800

    Updates

--- assets/review-expense-6d8fa7e7.js
+++
@@ -1,1 +0,0 @@
-import"./main-113e403d.js";import"./pie-chart-73ed3816.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
---
+++ assets/review-expense-e3d0da07.js
@@ -0,0 +1,1 @@
+import"./main-113e403d.js";import"./pie-chart-07d465d6.js";new Swiper(".category-top-3-swiper",{loop:!0,navigation:{nextEl:".swiper-button-custom-next",prevEl:".swiper-button-custom-prev"}});
